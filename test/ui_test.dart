60928a346dbefbea1c20dd978ee3b6831a3750a8
Eirik <eiriknd@stud.ntnu.no>
2022-12-04 17:31:38 +0100

    UI-tests suite

---
+++ test/ui_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:whats_in_my_fridge/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:whats_in_my_fridge/responsive/mobile_screen_layout.dart';
+import 'package:whats_in_my_fridge/screens/storage_page.dart';
+import 'package:whats_in_my_fridge/screens/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:whats_in_my_fridge/utilities/fire_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:whats_in_my_fridge/screens/login_page.dart';
+
+///UI-test suite for whats in my fridge application.
+void main() {
+
+}
