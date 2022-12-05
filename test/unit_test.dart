0b413843d3b47a52c9982fd85bdf547cb312f972
Eirik <eiriknd@stud.ntnu.no>
2022-12-05 12:32:32 +0100

    test validators
    name, email, password

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -11,8 +11,19 @@ import 'package:provider/provider.dart';
 import 'package:whats_in_my_fridge/utilities/fire_auth.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:whats_in_my_fridge/screens/login_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 
 ///UI-test suite for whats in my fridge application.
 void main() {
-
+  testWidgets('Search for product from api success', (tester) async {
+    final firestore = FakeFirebaseFirestore();
+    var user;
+    await tester.pumpWidget(HomePage(user: user));
+    await tester.enterText(find.byType(TextField), 'Apple');
+    await tester.tap(find.byType(InkWell));
+    await tester.pump();
+    expect(find.text('Apple'), findsAtLeastNWidgets(2));
+  });
 }
+
